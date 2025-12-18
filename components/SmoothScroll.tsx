c362814ce7399a8edd548847956b9550012f9fe6
Travis Johnson <78767139+travcjohnson@users.noreply.github.com>
2025-12-18 11:26:53 -0800

    fix: Tune smooth scroll settings for lighter feel and better responsiveness

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,13 +6,13 @@ import Lenis from "lenis";
 export default function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.0,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
+      wheelMultiplier: 1.5,
+      touchMultiplier: 3,
     });
 
     function raf(time: number) {
